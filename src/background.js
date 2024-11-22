e8064c2ff4a5698d37c8b0a1cd0c724f7b6f7a1d
Wilson Reyhan <jwreyhan@gmail.com>
2024-11-22 10:52:37 -0800

    cleaned up background.js

--- src/background.js
+++ src/background.js
@@ -4,30 +4,50 @@ let openai;
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type == "OPENAI_API_KEY_CHECK") {
-    console.log("Background received request:", request);
     chrome.storage.local.get(["OPENAI_API_KEY"], (result) => {
-      const OPENAI_API_KEY = result.OPENAI_API_KEY;
-      if (!OPENAI_API_KEY) {
-        console.error("Sending Missing Key response.");
-        sendResponse({ success: false, data: "MISSING_KEY" });
-        return true;
-      }
-      openai = new OpenAI({
-        apiKey: OPENAI_API_KEY,
-      });
-      try {
-        testConnection();
-        console.log("SUCCESSFUL_API_CONNECTION");
-        sendResponse({ success: true, data: "SUCCESSFUL_API_CONNECTION" });
-      } catch (error) {
-        console.error("INVALID_CREDENTIALS");
-        sendResponse({ success: false, data: "INVALID_CREDENTIALS" });
-      }
+      const success = handleAPIKey(result.OPENAI_API_KEY, sendResponse);
     });
-    return true;
+  } else if (request.type == "GENERATE_AI_RESPONSE") {
+    handleAIResponse(request.prompt, sendResponse);
   }
+  return true;
 });
 
+// handle OpenAI API Key.
+// Returns true if API connection is successful, otherwise false.
+function handleAPIKey(key, sendResponse) {
+  const OPENAI_API_KEY = key;
+
+  if (!OPENAI_API_KEY) {
+    console.error("Sending Missing Key response.");
+    sendResponse({ success: false, data: "MISSING_KEY" });
+    return false;
+  }
+
+  openai = new OpenAI({
+    apiKey: OPENAI_API_KEY,
+  });
+
+  try {
+    testConnection();
+    console.log("SUCCESSFUL_API_CONNECTION");
+    sendResponse({ success: true, data: "SUCCESSFUL_API_CONNECTION" });
+    return true;
+  } catch (error) {
+    console.error("INVALID_CREDENTIALS");
+    sendResponse({ success: false, data: "INVALID_CREDENTIALS" });
+    return false;
+  }
+}
+
+// Handle successful and unsuccessful AI response
+function handleAIResponse(prompt, sendResponse) {
+  generateAIResponse(prompt)
+    .then((response) => sendResponse({ success: true, data: response }))
+    .catch((error) => sendResponse({ success: false, data: error.message }));
+}
+
+// returns AI response to given prompt
 async function generateAIResponse(prompt) {
   const chatCompletion = await openai.chat.completions.create({
     messages: [{ role: "user", content: prompt }],
@@ -35,17 +55,7 @@ async function generateAIResponse(prompt) {
   });
   return chatCompletion;
 }
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type == "GENERATE_AI_RESPONSE") {
-    console.log("Background received request:", request);
-    generateAIResponse(request.prompt)
-      .then((response) => sendResponse({ success: true, data: response }))
-      .catch((error) => sendResponse({ success: false, data: error.message }));
-    return true;
-  }
-});
-
+// tests AI connection
 async function testConnection() {
   const chatCompletion = await openai.chat.completions.create({
     messages: [
