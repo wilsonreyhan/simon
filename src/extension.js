0ebf2cadb4728acce3dcc6bba82a2f401b0bbffa
Wilson Reyhan <jwreyhan@gmail.com>
2024-11-22 11:14:15 -0800

    cleaned up extension.js

--- src/extension.js
+++ src/extension.js
@@ -10,78 +10,76 @@ const loaderId = setInterval(() => {
   startExtension(window._gmailjs);
 }, 100);
 
-/* // parse HTML code to Text
-function htmlToText(htmlContent) {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(htmlContent, "text/html");
-  return doc.body.textContent.trim();
-} */
+// for future referece, if we want to print ai reponses
+// console.log("AI Response: " + response.data.choices[0].message.content);
 
+// logs messages sent from extension context in webpage
 window.addEventListener("message", function (event) {
   if (event.data.type == "FROM_EXTENSION_TO_WEBPAGE") {
-    const response = event.data.response;
-    if (response.success) {
-      // for printing chat responses
-      // console.log("AI Response: " + response.data.choices[0].message.content);
-
-      // for API Key testing
-      console.log(response);
-    } else {
-      console.error("Error: ", response.data);
-    }
+    logMessages(event.data.response);
   }
 });
 
+// launch extension
 function startExtension(gmail) {
-  console.log("Extension loading...");
   window.gmail = gmail;
 
   gmail.observe.on("load", () => {
-    const userEmail = gmail.get.user_email();
-    console.log("Hello, " + userEmail + ". This is Simon talking!");
-    window.postMessage(
-      {
-        type: "FROM_WEBPAGE_TO_EXTENSION",
-        message: {
-          type: "OPENAI_API_KEY_CHECK",
-        },
-      },
-      "*"
-    );
-
-    gmail.observe.on("view_email", (domEmail) => {
-      console.log("Looking at email:", domEmail);
-      const threadData = gmail.new.get.thread_data(domEmail);
-      const threadEmails = threadData.emails;
-      // const text = htmlToText(emailData.content_html);
-      console.log("Thread data:", threadData);
-      console.log("Thread Emails: ", threadEmails);
-    });
-
-    gmail.observe.on("compose", (compose) => {
-      console.log("New compose window is opened!", compose);
-      console.log("Compose Ref", gmail.dom.compose());
-
-      const parrotUrl = document.getElementById("extension-url-container")
-        .dataset.parrotUrl;
-
-      gmail.tools.add_compose_button(
-        compose,
-        `<img src="${parrotUrl}" style="width: 20px; height: 20px;" alt="Parrot Icon" />`,
-        async () => {
-          window.postMessage(
-            {
-              type: "FROM_WEBPAGE_TO_EXTENSION",
-              message: {
-                type: "GENERATE_AI_RESPONSE",
-                prompt: "Say this is a test.",
-              },
-            },
-            "*"
-          );
-        },
-        ""
-      );
-    });
+    intialAPICheck();
   });
+
+  gmail.observe.on("view_email", (domEmail) => {
+    // vars for future reference
+    const threadData = gmail.new.get.thread_data(domEmail);
+    const threadEmails = threadData.emails;
+  });
+
+  gmail.observe.on("compose", (compose) => {
+    addSimonButton(gmail, compose);
+  });
+}
+
+// initialize API Key verification process
+function intialAPICheck() {
+  window.postMessage(
+    {
+      type: "FROM_WEBPAGE_TO_EXTENSION",
+      message: {
+        type: "OPENAI_API_KEY_CHECK",
+      },
+    },
+    "*"
+  );
+}
+
+// add Simon button to gmail compose window
+function addSimonButton(gmail, compose) {
+  const parrotUrl = document.getElementById("extension-url-container").dataset
+    .parrotUrl;
+
+  gmail.tools.add_compose_button(
+    compose,
+    `<img src="${parrotUrl}" style="width: 20px; height: 20px;" alt="Parrot Icon" />`,
+    async () => {
+      window.postMessage(
+        {
+          type: "FROM_WEBPAGE_TO_EXTENSION",
+          message: {
+            type: "GENERATE_AI_RESPONSE",
+            prompt: "Say this is a test.",
+          },
+        },
+        "*"
+      );
+    },
+    ""
+  );
+}
+// log messages
+function logMessages(message) {
+  if (message.success) {
+    console.log(message);
+  } else {
+    console.error("Error: ", message.data);
+  }
 }
